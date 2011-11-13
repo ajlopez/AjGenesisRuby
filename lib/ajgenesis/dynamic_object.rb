ec51f65b7ba19a1ff0f457495cb5c761b1552738
ajlopez <webmaster@ajlopez.com>
2011-11-13 17:56:31 -0300

    dynamic_object.rb tabs replaces by spaces, again

--- lib/ajgenesis/dynamic_object.rb
+++ lib/ajgenesis/dynamic_object.rb
@@ -2,20 +2,21 @@
 module AjGenesis
 
 class DynamicObject
-	def initialize
-		@values = {}
-	end
-	
-	def method_missing(*args)
-		if args.length == 1
-			@values[args[0]]
-		elsif args.length == 2 and args[0].to_s =~ /^(.*)=$/
-			@values[$1.intern] = args[1]
-		else
-			super
-		end
-    end	
+    def initialize
+        @values = {}
+    end
+    
+    def method_missing(*args)
+        if args.length == 1
+            @values[args[0]]
+        elsif args.length == 2 and args[0].to_s =~ /^(.*)=$/
+            @values[$1.intern] = args[1]
+        else
+            super
+        end
+    end    
 end
 
 end
+
 
